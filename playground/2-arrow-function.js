e130e933bf9e74049684324262a3c86ebf9290bc
Yogesh Walia <ywalia@sapient.com>
2019-11-05 14:57:13 +0000

    2-arrow-function.js

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -23,3 +23,28 @@ const event = {
 }
 
 event.printGuestList()
+
+const test = {
+    x: 12,
+    yList: [4, 5, 3],
+    z: 8,
+    m: function () {
+        console.log(this.x)
+    },
+    n: () => console.log(this.x),
+    p() {
+        console.log(this.x)
+        this.yList.forEach(function () {
+            console.log(this.z)
+        })
+    },
+    q() {
+        console.log(this.z)
+        this.yList.forEach( () => console.log(this.z))
+    }
+}
+
+test.m()
+test.n()
+test.p()
+test.q()
